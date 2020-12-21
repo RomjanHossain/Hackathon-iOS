b73f96e2dcc76190473555f7175630342aedbe82
RomjanHossain <romjanhossain726526@gmail.com>
2020-12-21 09:44:21 +0600

    add to fav

--- lib/screens/detail/detail.dart
+++ lib/screens/detail/detail.dart
@@ -188,7 +188,7 @@ class _DetailState extends State<Detail> {
                                           icon: Icon(
                                             Icons.remove,
                                           ),
-                                          label: Text('Remove from Favourites'),
+                                          label: Text('Remove'),
                                         )
                                       : FlatButton.icon(
                                           color: Colors.white60,
